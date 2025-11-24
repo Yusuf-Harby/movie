c6a410c88233d0e6cd7bcc89b2f9c7d1928468ee
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-24 16:45:36 +0200

    chore: add and organize application constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF242A32);
+  static const Color selected = Color(0xFF0296E5);
+  static const Color gray = Color(0xFF67686D);
+  static const Color white = Color(0xFFFFFFFF);
+}
