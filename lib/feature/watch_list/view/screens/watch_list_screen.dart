96e28e795a4def5757568a78ed2402c3e91409e8
Yusuf Harby <youssef.srour789@gmail.com>
2025-11-27 23:35:28 +0200

    feat(watch-list): feature ui
    - finished watchlist ui

--- lib/feature/watch_list/view/screens/watch_list_screen.dart
+++ lib/feature/watch_list/view/screens/watch_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie/core/constants/app_assets.dart';
 import 'package:movie/core/constants/app_strings.dart';
 
 class WatchListScreen extends StatelessWidget {
@@ -6,8 +7,30 @@ class WatchListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> watchList = [];
     return Scaffold(
       appBar: AppBar(title: const Text(AppStrings.watchList)),
+      body: watchList.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.noSavedMovies),
+                  const SizedBox(height: 16),
+                  Text(
+                    'There is no movie yet!',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Find your movie by Type title,\ncategories, years, etc ',
+                    style: Theme.of(context).textTheme.bodySmall,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          : null,
     );
   }
 }
